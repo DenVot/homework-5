4d4e1b868329bb65b7ee822d9b25d449befd9ee9
Denis Voitenko <denis@denvot.dev>
2023-12-13 19:04:33 +0300

    Реализация сервиса продуктов

--- src/main/java/org/homework/services/ProductServiceBase.java
+++ src/main/java/org/homework/services/ProductServiceBase.java
@@ -1,6 +1,8 @@
 package org.homework.services;
 
+import org.homework.exceptions.ProductNotFoundException;
+
 public interface ProductServiceBase {
-  void increaseProduct(String name, int amount);
-  void decreaseProduct(String name, int amount);
+  void increaseProduct(String name, int amount) throws ProductNotFoundException;
+  void decreaseProduct(String name, int amount) throws ProductNotFoundException;
 }
