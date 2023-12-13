b93b4943f12544d747bbb41a48d76b262429af86
Denis Voitenko <denis@denvot.dev>
2023-12-13 20:39:14 +0300

    Проверка на негативные значения

--- src/main/java/org/homework/repositories/ProductRepository.java
+++ src/main/java/org/homework/repositories/ProductRepository.java
@@ -1,9 +1,10 @@
 package org.homework.repositories;
 
+import org.homework.exceptions.NegativeProductCountException;
 import org.homework.exceptions.ProductNotFoundException;
 
 public interface ProductRepository {
-  void increaseProduct(String name, int amount) throws ProductNotFoundException;
+  void increaseProduct(String name, int amount) throws ProductNotFoundException, NegativeProductCountException;
 
-  void decreaseProduct(String name, int amount) throws ProductNotFoundException;
+  void decreaseProduct(String name, int amount) throws ProductNotFoundException, NegativeProductCountException;
 }
